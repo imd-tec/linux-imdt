c4ba910a9ef5f8bf413e6fb85c2c104cc4f35426
William Bright <William.Bright@imd-tec.com>
2025-05-30 15:25:42 +0100

    Added bayer format support into the ISI driver

--- drivers/staging/media/imx/imx8-isi-fmt.c
+++ drivers/staging/media/imx/imx8-isi-fmt.c
@@ -97,6 +97,34 @@ struct mxc_isi_fmt mxc_isi_out_formats[] = {
 		.colplanes	= 1,
 		.align		= 2,
 		.mbus_code	= MEDIA_BUS_FMT_RGB888_1X24,
+	},
+	{
+		.name		= "BGGR10",
+		.fourcc		= V4L2_PIX_FMT_SBGGR10,
+		.depth		= { 16 },
+		.color		= MXC_ISI_OUT_FMT_RAW10,
+		.memplanes	= 1,
+		.colplanes	= 1,
+		.align = 2,
+		.mbus_code	= MEDIA_BUS_FMT_SBGGR10_1X10,
+	}, {
+		.name		= "GRBG10",
+		.fourcc		= V4L2_PIX_FMT_SGRBG10,
+		.depth		= { 16 },
+		.color		= MXC_ISI_OUT_FMT_RAW10,
+		.memplanes	= 1,
+		.colplanes	= 1,
+		.align = 2,
+		.mbus_code	= MEDIA_BUS_FMT_SGRBG10_1X10,
+	}, {
+		.name		= "GRBG12",
+		.fourcc		= V4L2_PIX_FMT_SGRBG12,
+		.depth		= { 16 },
+		.color		= MXC_ISI_OUT_FMT_RAW12,
+		.memplanes	= 1,
+		.colplanes	= 1,
+		.align = 2,
+		.mbus_code	= MEDIA_BUS_FMT_SGRBG12_1X12,
 	}
 };
 
